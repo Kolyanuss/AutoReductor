509222724f39ba3bfeff6e270ff1c280a73f0f25
0xKolyanus <mukola.1zt.@gmail.com>
2024-06-22 12:17:04 +0300

    added min\max\step in reduction

--- AutoReductor.py
+++ AutoReductor.py
@@ -4,21 +4,23 @@ from Data import DataLoader, DataPreproces
 from OptimalParametersSelector import OptimalParametersSelector
 import matplotlib.pyplot as plt
 import pandas as pd
+import math
 import os
 from MyReductionModels import Autoencoder
 
 def plot(df, path_to_save=None, file_name=None):
-    fig, ax1 = plt.subplots(figsize=(8, 4))    
+    fig, ax1 = plt.subplots(figsize=(10, 4))    
     ax2 = ax1.twinx()
     ax3 = ax1.twinx()
     ax3.spines['right'].set_position(('outward', 60))
     
-    x_layer = df.iloc[:, :-3].apply(lambda x: "_".join(x.astype(str)), axis=1)
+    x_layer = df.iloc[:, :-3].apply(lambda x: ",".join(x.astype(str)), axis=1) # combination of params
     ax1.plot(x_layer, df['mean_test_score'], 'g-')
+    ax1.set_xticklabels(x_layer, rotation=90)
     ax2.plot(x_layer, df['mean_fit_time'], 'b-')
     ax3.plot(x_layer, df['mean_score_time'], 'r-')
 
-    ax1.set_xlabel(' '.join(df.columns))
+    ax1.set_xlabel(' + '.join(df.iloc[:, :-3].columns))
     ax1.set_ylabel('Eval_model_accuracy', color='g')
     ax2.set_ylabel('fit_time', color='b')
     ax3.set_ylabel('score_time', color='r')
@@ -39,25 +41,39 @@ def save_fig(path_to_save,file_name):
 
 class AutoReductor():
     results_folder = "results"
-    #todo: min_reduction, max_reduction
-    reduction_model_dict = {
-        'AE': (Autoencoder(), { 'AE__lat_dim_ae': list(range(10,110,10)) } ),
-        'SVD': (TruncatedSVD(), {'SVD__n_components': list(range(10,160,10))} )
-        }
     evaluation_model_list = [MLPClassifier(solver="lbfgs")]
     
-    def __init__(self) -> None:
-        pass
+    def __init__(self, min_reduction = 5, max_reduction = 20, step_count = 10) -> None:
+        dl = DataLoader(DataLoader.load_mnist) # + інші датасети + можливість вибирати датасет
+        self.data = dl.get_gata()
+        
+        original_dimm = math.prod(self.data[0].shape[1:3])
+        
+        svd_min = round(math.floor(original_dimm / max_reduction), -1)
+        if svd_min < 10:
+            svd_min = 10
+            
+        svd_max = round(math.floor(original_dimm / min_reduction), -1)
+        if svd_max > original_dimm:
+            svd_max = original_dimm
+        
+        svd_step = round(math.floor((svd_max-svd_min) / step_count), 0)
+        
+        self.reduction_model_dict = {
+            'AE': (Autoencoder(), { 'AE__lat_dim_ae': list(range(10,110,40)) } ),
+            # 'SVD': (TruncatedSVD(), {'SVD__n_components': list(range(10,160,40))} )
+            'SVD': (TruncatedSVD(), {'SVD__n_components': list(range(svd_min, svd_max, svd_step))} )
+            }
     
     def start(self):
-        dl = DataLoader(DataLoader.load_mnist) # + інші датасети + можливість вибирати датасет
-        data = dl.get_gata()
-        data = DataPreproces.normalize_x(data)
-        # data = DataPreproces.unwrapper(data)
-        print(data[0].shape)
+        
+        
+        self.data = DataPreproces.normalize_x(self.data)
+        # self.data = DataPreproces.unwrapper(self.data)
+        print(self.data[0].shape)
         
         for eval_modle in self.evaluation_model_list:
-            ops = OptimalParametersSelector(data, self.reduction_model_dict, eval_modle)
+            ops = OptimalParametersSelector(self.data, self.reduction_model_dict, eval_modle)
             ops.find_optimal_param()
             result = ops.get_result()
             save_path = os.path.join(self.results_folder, str(eval_modle))
