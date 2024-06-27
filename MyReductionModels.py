4537777d2ce115c44d7fadee1188d04181693158
0xKolyanus <mukola.1zt.@gmail.com>
2024-06-27 06:26:24 +0300

    flexible input shape 4 autoencoder

--- MyReductionModels.py
+++ MyReductionModels.py
@@ -1,3 +1,4 @@
+import math
 from sklearn.base import BaseEstimator, TransformerMixin
 from tensorflow.keras.layers import Input, Dense, Flatten, Reshape, Conv2D, MaxPooling2D, UpSampling2D
 from tensorflow.keras.models import Model
@@ -5,7 +6,7 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.losses import MeanSquaredError
 
 class Autoencoder(BaseEstimator, TransformerMixin):
-    def __init__(self, input_shape = (28, 28, 1),  lat_dim_ae=30):
+    def __init__(self, input_shape,  lat_dim_ae=30):
         self.lat_dim_ae = lat_dim_ae
         self.input_shape = input_shape
         self.autoencoder = None
@@ -19,13 +20,19 @@ class Autoencoder(BaseEstimator, TransformerMixin):
         x = Flatten()(x)
         encoded = Dense(self.lat_dim_ae)(x)
 
-        x = Dense(7 * 7 * 64)(encoded)
-        x = Reshape((7, 7, 64))(x)
+        # Calculate the shape after flattening
+        conv_output_shape = (self.input_shape[0] // 4, self.input_shape[1] // 4, 64)
+        dense_units = math.prod(conv_output_shape)
+        
+        x = Dense(dense_units)(encoded)
+        x = Reshape(conv_output_shape)(x)
+        # x = Dense(7 * 7 * 64)(encoded)
+        # x = Reshape((7, 7, 64))(x)        
         x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
         x = UpSampling2D((2, 2))(x)
         x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
         x = UpSampling2D((2, 2))(x)
-        decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+        decoded = Conv2D(self.input_shape[2], (3, 3), activation='sigmoid', padding='same')(x)
 
         self.autoencoder = Model(inputs, decoded)
         self.autoencoder.compile(optimizer=Adam(), loss=MeanSquaredError())
