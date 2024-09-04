22e8cd7e7ae54c02e4a21544308124295334fcee
Nikolay_Laptop <mukola.1zt@gmail.com>
2024-09-04 12:13:08 +0300

    add custom dataset loading func

--- Data.py
+++ Data.py
@@ -1,9 +1,13 @@
 import numpy as np
 import tensorflow as tf
+import os
+import numpy as np
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
 class DataLoader():
     def __init__(self):
-        self.dataset_dict = {"mnist": self.__mnist, "fashion_mnist": self.__fashion_mnist, "cifar10": self.__cifar10 }
+        self.dataset_dict = {"mnist": self.__mnist, "fashion_mnist": self.__fashion_mnist, "cifar10": self.__cifar10, "Import custom dataset": self.__import_custom_dataset }
         
     def get_datasets_names(self):
         return list(self.dataset_dict.keys())
@@ -20,6 +24,26 @@ class DataLoader():
         (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
         return (x_train, y_train.ravel(), x_test, y_test.ravel())
     
+    def __import_custom_dataset(self, folder_path, test_size=0.2):
+        x = []
+        y = []
+        
+        for class_name in os.listdir(folder_path):
+            class_folder_path = os.path.join(folder_path, class_name)
+            
+            if not os.path.isdir(class_folder_path):
+                continue
+            
+            for image_name in os.listdir(class_folder_path):
+                image_path = os.path.join(class_folder_path, image_name)
+                img = load_img(image_path)
+                x.append(img_to_array(img))
+                y.append(class_name)  # припускаємо, що назва папки — це клас
+
+        x = np.array(x)
+        y = np.array(y)
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size, random_state=42, stratify=y)
+        return (x_train, y_train, x_test, y_test)
     
     def get_data_by_name(self, name):
         try:
