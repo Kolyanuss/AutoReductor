b3fe687a339a76acd38892100798e75faa3b475e
0xKolyanus <mukola.1zt.@gmail.com>
2024-12-01 11:17:12 +0200

    add LogisticRegression to the evaluation models

--- MyEvaluationModels.py
+++ MyEvaluationModels.py
@@ -2,10 +2,12 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
 
-evaluation_model_list = (MLPClassifier(solver="lbfgs"), SVC(kernel='linear'), DecisionTreeClassifier(), RandomForestClassifier())
+evaluation_model_list = (LogisticRegression(), MLPClassifier(solver="lbfgs"), SVC(kernel='linear'), DecisionTreeClassifier(), RandomForestClassifier())
 
 def get_eval_model_by_name(name):
     for model in evaluation_model_list:
         if name == str(model):
             return model
+        
